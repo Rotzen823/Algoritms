47fc4eecc839922ed4578172a478223a4962855c
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2023-07-09 00:19:00 -0700

    Create Bellman_Ford.cpp

---
+++ Grafos/Bellman_Ford.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define INF 100000000
+
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+typedef struct Edges{
+    int a;
+    int b;
+    int c;
+}Edges;
+
+struct Grafo{
+    int V;
+    vector<Edges> edges;
+    vi dist;
+
+    void init(int _V){
+        V = _V;
+    }
+
+    void ae(int a, int b, int c){ edges.push_back({a, b, c}); }
+
+    void Bellman_Ford(int x){
+        dist.assign(V, INF);
+
+        dist[x] = 0;
+        bool bandera = true;
+        while (bandera) {
+            bandera = false;
+            for (auto e : edges) {
+                int a, b, w;
+                a = e.a; b = e.b; w = e.c;
+                if((dist[a] + w) < dist[b]){
+                    dist[b] = dist[a] + w;
+                    bandera = true;
+                }
+            }
+        }
+    }
+};
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    Grafo g;
+    g.init(n);
+
+    while(m--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        g.ae(a, b, c);
+        g.ae(b, a, c);
+    }
+
+    g.Bellman_Ford(0);
+    for(int k = 0; k < n; k++){
+        cout << "0 -> " << k << ": " << g.dist[k] << "\n";
+    }
+
+    return 0;
+}
