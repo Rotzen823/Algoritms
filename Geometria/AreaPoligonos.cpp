dccadf1ada4bfcdf555c8bd8604b54d0b76ff614
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2023-10-01 21:03:10 -0700

    Create AreaPoligonos.cpp

---
+++ Geometria/AreaPoligonos.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+vii puntos;
+int sacarArea(vii &puntos){
+    int n = puntos.size();
+    int area = 0;
+    for(int k = 0; k < n; k++){
+        int j = (k + 1) % n;
+        area += puntos[k].first * puntos[j].second - puntos[j].first * puntos[k].second;
+    }
+
+    return abs(area) / 2;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    for(int k = 0; k < n; k++){
+        int x, y;
+        cin >> x >> y;
+        puntos.push_back(ii(x, y));
+    }
+
+    cout << sacarArea(puntos);
+  
+
+  return 0;
+}
