95668992d5b441ecb1e7830e00c21013b0c7beb0
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2023-11-01 23:34:31 -0700

    Create FFT.cpp

---
+++ Matematicas/FFT.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+using cd = complex<double>;
+const double PI = acos(-1);
+
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
+    if (n == 1)
+        return;
+
+    vector<cd> a0(n / 2), a1(n / 2);
+    for (int i = 0; 2 * i < n; i++) {
+        a0[i] = a[2*i];
+        a1[i] = a[2*i+1];
+    }
+    fft(a0, invert);
+    fft(a1, invert);
+
+    double ang = 2 * PI / n * (invert ? -1 : 1);
+    cd w(1), wn(cos(ang), sin(ang));
+    for (int i = 0; 2 * i < n; i++) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n/2] = a0[i] - w * a1[i];
+        if (invert) {
+            a[i] /= 2;
+            a[i + n/2] /= 2;
+        }
+        w *= wn;
+    }
+}
+
+vi multiply(vi const& a, vi const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < (int) (a.size() + b.size())) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vi result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cout.setf(ios::fixed); cout << setprecision(2);
+    
+    int n, m;
+    cin >> n >> m;
+    vector<int> p1(n), p2(m);
+    for(int k = 0; k < n; k++){
+        int x;
+        cin >> x;
+        p1[k] = x;
+    }
+
+    for(int k = 0; k < m; k++){
+        int x;
+        cin >> x;
+        p2[k] = x;
+    }
+
+    vi res = multiply(p1, p2);
+
+    for(int k = 0; k < (int) res.size(); k++){
+        cout << res[k] << " ";
+    }
+
+    return 0;
+}
